7324db3f5052c0947fd90ed1b2e0486fbcb1c9f5
syh053 <syh053@gmail.com>
2026-07-25 16:39:25 +0800

    feat(main): 前台餐廳資料新增餐廳類別欄位資料

--- src/service/front_service/restaurant.py
+++ src/service/front_service/restaurant.py
@@ -3,6 +3,7 @@ from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.model import Restaurant
+from db.model.category import Category
 from src.vm.end.restaurant_vm import EndRestaurantGetReqModel, EndRestaurantRespModel
 
 
@@ -12,13 +13,14 @@ class GetRestaurant:
 
     async def get_all_restaurant(self, params: EndRestaurantGetReqModel) -> tuple[list[EndRestaurantRespModel], int]:
         stmt = (
-            select(Restaurant, func.count(Restaurant.id).over().label("total"))
+            select(Restaurant, Category.name.label("category_name"), func.count(Restaurant.id).over().label("total"))
             .select_from(Restaurant)
+            .outerjoin(Category, Category.id == Restaurant.category_id)
             .where_if(params.name, lambda: Restaurant.name.ilike(f"%{params.name}%"))
             .where_if(params.tel, lambda: Restaurant.tel.ilike(f"%{params.tel}%"))
             .where_if(params.openingHours, lambda: Restaurant.openingHours.ilike(f"%{params.openingHours}%"))
             .where_if(params.address, lambda: Restaurant.address.ilike(f"%{params.address}%"))
-            .where_if(params.description, lambda :Restaurant.description.ilike(f"%{params.description}%"))
+            .where_if(params.description, lambda: Restaurant.description.ilike(f"%{params.description}%"))
             .offset((params.current_page - 1) * params.page_size)
             .limit(params.page_size)
         )
@@ -26,7 +28,9 @@ class GetRestaurant:
 
         results = results.fetchall()
 
-        datas = [EndRestaurantRespModel.model_validate(result[0]) for result in results]
-        total = results[0][1] if results else 0
+        datas = [EndRestaurantRespModel.model_validate(result[0]).model_copy(
+            update={"category_name": result[1]}
+        ) for result in results]
+        total = results[0][2] if results else 0
 
         return datas, total
